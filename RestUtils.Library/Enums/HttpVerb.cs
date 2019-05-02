9317bb70feda2c5fcaea97564b0d9701203ab9a9
Peter Centellini <peter.c@trivec.se>
2019-05-02 18:45:58 +0200

    Initial Commit

---
+++ RestUtils.Library/Enums/HttpVerb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestUtils.Library.Enums {
+  public enum HttpVerb {
+    GET,
+    POST,
+    PUT,
+    DELETE
+  }
+}
